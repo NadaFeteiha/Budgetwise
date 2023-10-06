fdce284f6237ec4ed34f268f030e43e1d8792160
Nada Feteiha <nada.feteiha@gmail.com>
2023-10-06 00:00:30 -0700

    add splash and launcher icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,18 +74,9 @@ dependencies {
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
 
-
-    //Dagger - Hilt
-//    implementation ("com.google.dagger:hilt-android:2.40.5")
-
-//
-//    kapt ("androidx.hilt:hilt-compiler:1.0.0")
-//    kapt ("com.google.dagger:hilt-android-compiler:2.40.5")
-
     //Dagger - Hilt
     implementation("com.google.dagger:hilt-android:2.48")
     ksp("com.google.dagger:hilt-compiler:2.48")
-//    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha01")
     implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
 
     // Room
@@ -99,5 +90,8 @@ dependencies {
 
     //Navigation
     implementation (libs.androidx.navigation.compose)
+
+    //Splash
+    implementation(libs.androidx.core.splashscreen)
 
 }
--- app/src/main/java/com/nadafeteiha/budgetwise/MainActivity.kt
+++ app/src/main/java/com/nadafeteiha/budgetwise/MainActivity.kt
@@ -1,52 +1,33 @@
 package com.nadafeteiha.budgetwise
 
-import android.content.res.Resources.Theme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Add
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.nadafeteiha.budgetwise.ui.composable.BottomBar
 import com.nadafeteiha.budgetwise.ui.composable.BottomNavigationScreens
 import com.nadafeteiha.budgetwise.ui.composable.NavigationGraph
-import com.nadafeteiha.budgetwise.ui.composable.ROUTE_CATEGORY
 import com.nadafeteiha.budgetwise.ui.theme.BudgetwiseTheme
 import com.nadafeteiha.budgetwise.ui.theme.LocalNavController
-import com.nadafeteiha.budgetwise.ui.theme.Purple80
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
